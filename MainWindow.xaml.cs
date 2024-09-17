0cb616b3ef826a2ae3f75d732146521807942833
arpi <totharpi18@gmail.com>
2024-09-17 11:23:05 +0200

    administration

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -127,5 +127,55 @@ namespace ellenorzo
             txtDormitory.Clear();
         }
 
+        //Administration
+        private void btnAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            dgStudents.ItemsSource = null;
+            dgStudents.ItemsSource = students;
+
+            var boarderCount = students.Count(x => x.IsBoarder);
+            var debrecenCount = students.Count(x => x.Address.Contains("Debrecen"));
+            var commuterCount = students.Count(x => !x.IsBoarder);
+            var majorStat = students.GroupBy(x => x.Major)
+                                           .Select(x => new { Major = x.Key, StudentCount = x.Count() })
+                                           .ToList();
+
+            string stats = $"Kollégisták száma: {boarderCount}\nBejárósok száma: {commuterCount}\nDebreceni tanulók száma: {debrecenCount}\n\nSzakok szerinti statisztika:\n";
+
+            foreach (var stat in majorStat)
+            {
+                stats += $"{stat.Major}: {stat.StudentCount} tanuló\n";
+            }
+
+            MessageBox.Show(stats, "Statisztika", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void DeleteStudent_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var student = button?.Tag as Student;
+
+            if (student != null)
+            {
+                var result = MessageBox.Show($"Biztosan törölni szeretnéd {student.Name} adatait?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    students.Remove(student);
+                    SaveStudents();
+
+                    dgStudents.ItemsSource = null;
+                    dgStudents.ItemsSource = students;
+
+                    MessageBox.Show($"{student.Name} sikeresen törölve lett!", "Törlés", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
     }
 }

9b469c1929be858edb2da37676e2584ae6af0378
arpi <totharpi18@gmail.com>
2024-09-17 11:19:01 +0200

    student class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ellenorzo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        //Student class
+        public class Student
+        {
+            public string Name { get; set; }
+            public string BirthPlace { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string MotherName { get; set; }
+            public string Address { get; set; }
+            public DateTime EnrollmentDate { get; set; }
+            public string Major { get; set; }
+            public string Class { get; set; }
+            public bool IsBoarder { get; set; }
+            public string Dormitory { get; set; }
+            public string LogNumber { get; set; }
+            public string RegistrationNumber { get; set; }
+        }
+
     }
 }
